32f7d5269eb74c00a631dcc11abfe788d87b6200
Ian Main <imain@redhat.com>
2017-08-25 18:58:39 +0000

    Change to master.

--- overcloud-prepare.sh
+++ overcloud-prepare.sh
@@ -1,19 +1,8 @@
-### --start_docs
-## Prepare the undercloud for deploying the containerized compute node
-## ===================================================================
-
-## .. note:: In progress documentation is available at https://etherpad.openstack.org/p/tripleo-containers-work
-## ::
-
-## Prepare Your Environment
-## ------------------------
-
-## * Populate the docker registry
-## ::
+#!/bin/bash
 
 openstack overcloud container image prepare \
     --images-file /home/stack/overcloud_containers.yaml \
-    --namespace tripleo \
+    --namespace master \
     --tag tripleo-ci-testing \
     --pull-source trunk.registry.rdoproject.org \
     --exclude ceph \
@@ -22,25 +11,11 @@ openstack overcloud container image prepare \
 openstack overcloud container image upload --verbose --config-file /home/stack/overcloud_containers.yaml
 
 
-## * Configure the /home/stack/containers-default-parameters.yaml, this is done automatically.
+## * Configure the /home/jenkins/containers-default-parameters.yaml, this is done automatically.
 ## ::
 
 openstack overcloud container image prepare \
     --env-file /home/stack/containers-default-parameters.yaml \
     --tag tripleo-ci-testing \
-    --namespace 192.168.24.1:8787/tripleo
+    --namespace 192.168.24.1:8787/master
 
-echo "  DockerInsecureRegistryAddress: 192.168.24.1:8787" >> \
-    /home/stack/containers-default-parameters.yaml
-
-echo "============================="
-echo "Containers default parameters:"
-cat /home/stack/containers-default-parameters.yaml
-echo "============================="
-
-## * Get the journal logs for docker
-## ::
-
-sudo journalctl -u docker > docker_journalctl.log
-
-### --stop_docs
